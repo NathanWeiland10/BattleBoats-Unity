b9eb4648ea847340cecb3da041e1b43b931dc83e
Nathan Weiland <77362517+NathanWeiland10@users.noreply.github.com>
2022-06-13 08:31:45 -0400

    Added Tooltip

--- Assets/Scripts/ButtonSoundEffect.cs
+++ Assets/Scripts/ButtonSoundEffect.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ButtonSoundEffect : MonoBehaviour
 {
+    [Tooltip("The sound effect that will play once a button is pressed (needs to be set up in the Button component events menu)")]
     public string soundEffect;
 
     public void PlaySoundEffect()
     {
-        // Play sound:
         FindObjectOfType<AudioManager>().Play(soundEffect);
     }
 }
