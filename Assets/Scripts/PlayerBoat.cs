1d945666ace2cf4bdaad14526c208e2066891500
Nathan Weiland <77362517+NathanWeiland10@users.noreply.github.com>
2022-06-14 12:07:52 -0400

    Fixed CurrentEnemy Getter

--- Assets/Scripts/PlayerBoat.cs
+++ Assets/Scripts/PlayerBoat.cs
@@ -80,7 +80,7 @@ public class PlayerBoat : MonoBehaviour
     // These variables were switched to private, so they may need getters or setters later:
     // ----
     List<PlayerBoat> encounteredEnemies = new List<PlayerBoat>();
-    public PlayerBoat currentEnemy = null; // CHANGE TO A GETTER METHOD LATER (FIX LATER)
+    PlayerBoat currentEnemy = null;
     PlayerBase encounteredBase = null;
     bool isCapturing;
     // ----
@@ -228,6 +228,11 @@ public class PlayerBoat : MonoBehaviour
     public bool GetBoatFriendlyStatus()
     {
         return friendlyBoat;
+    }
+
+    public PlayerBoat getCurrentEnemy()
+    {
+        return currentEnemy;
     }
 
     public void AddEncounteredEnemy(PlayerBoat enemy)
